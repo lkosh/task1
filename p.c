b7f0e88b66430cdd6a64e611bfacfd7139c946f9
elena kosheleva <lkosh96@mail.ru>
2016-10-23 01:57:37 +0300

    commit #2

--- p.c
+++ p.c
@@ -2,11 +2,13 @@
 #include <time.h>
 #include <stdlib.h>
 #include <pthread.h>
+
 #define SIZE 10000
 double a[SIZE][SIZE];
 int i;
 double  b[SIZE][SIZE];
 double  c[SIZE][SIZE];
+typedef double __attribute((aligned(128))) al128d_t;
 int thread1_exited, thread2_exited;
 void *thread1_start(void *arg){
 	int errflag;
@@ -39,6 +41,7 @@ void *thread2_start(void *arg){
 }
 void main( int argc, char **argv )
 {
+// initialising matrixes
 	int i = 0, j = 0;
 	for ( i = 0; i < SIZE; i++)
 		for ( j = 0; j < SIZE; j ++){
@@ -49,6 +52,7 @@ void main( int argc, char **argv )
 	clock_t begin;
 	clock_t end;
   int k;
+//no optimisation 
   begin = clock();
   
 	  for( j = 0; j < SIZE; j++ ) {
@@ -62,7 +66,7 @@ void main( int argc, char **argv )
   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
   printf("Execution time is :%f seconds \n", time_spent);
 
-
+// optimisation step 1
 	begin = clock();
 	for (i = 0; i < SIZE; i ++){
 		for (j = 0; j < SIZE; j ++){
@@ -71,17 +75,24 @@ void main( int argc, char **argv )
 	}
 	end = clock();
 	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("Execution time after optimisation is :%f seconds \n", time_spent);
-	
-	
-	
-	
-	for ( i = 0; i < SIZE; i++)
-		for ( j = 0; j < SIZE; j ++){
-			b[i][j] = 1;
-			c[i][j] = 2;
-			a[i][j] = 0;
+	printf("Execution time after optimisation-1 is :%f seconds \n", time_spent);
+//optimisation step 2
+int step = 5;
+begin = clock();
+	for (i = 0; i < SIZE; i ++){
+		for (j = 0; j < SIZE - step; j += step){
+			a[i][j] = b[i][j] + c[i][j];
+			a[i][j+1] = b[i][j+1] + c[i][j+1];
+			a[i][j+2] = b[i][j+2] + c[i][j+2];
+			a[i][j+3] = b[i][j+3] + c[i][j+3];
+			a[i][j+4] = b[i][j+4] + c[i][j+4];
+			
 		}
+	}
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Execution time after optimisation 2 is :%f seconds \n", time_spent);
+// optimisation with threads
 	begin = clock();
 	int errflag;
 	pthread_t thread1, thread2;
@@ -96,24 +107,10 @@ void main( int argc, char **argv )
 		printf("Error creating a thread\n");
 		exit(1);
 	}
-	int fl = 1;
+
+	while (!thread1_exited || !thread2_exited){}
 	end = clock();
 	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	printf("Execution time after optimisation with threads is :%f seconds \n", time_spent);
-	while (!thread1_exited || !thread2_exited){}
-	/*for (i = 0; i < SIZE; i ++){
-		for (j = 0; j < SIZE; j ++){
-		
-				printf (" %01.0f",  a[i][j]);
-			}
-		printf("\n");
-			}
-		
-	if (fl)
-		printf("ok ! \n");
-	else printf ("%f", a[0][0]);
-		
-	
-	*/
 	
 }
